0fe347cdb147b550c8f6b793b51875d68d08cbc4
Elena Ter-Akopova <testfrog@yandex.ru>
2023-06-10 21:37:25 +0300

    Исправлены данные в фикстуре и тестах

--- Test/test_for_lesson2.py
+++ Test/test_for_lesson2.py
@@ -3,7 +3,7 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
-def test_for_search_string_true(browser_config):
+def test_for_search_string_true():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
